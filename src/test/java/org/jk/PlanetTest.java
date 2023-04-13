d3bd71c758f34721e46edbfd8b88b1e8ef366ce4
Jekabs <jekabs.kalpins@gmail.com>
2023-04-14 01:30:53 +0300

    Added SoftAssertions

--- src/test/java/org/jk/PlanetTest.java
+++ src/test/java/org/jk/PlanetTest.java
@@ -1,7 +1,7 @@
 package org.jk;
 
 import io.qameta.allure.testng.AllureTestNg;
-import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.jk.client.APIClient;
 import org.jk.utils.JSONParser;
 import org.springframework.http.HttpStatus;
@@ -34,15 +34,16 @@ public class PlanetTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         Planet expectedPlanet = JSONParser.parseJSONToObject(response.getBody(), Planet.class);
-
-        Assertions.assertThat(expectedPlanet.getName()).isEqualTo("Tatooine");
-        Assertions.assertThat(expectedPlanet.getRotationPeriod()).isEqualTo("23");
-        Assertions.assertThat(expectedPlanet.getOrbitalPeriod()).isEqualTo("304");
-        Assertions.assertThat(expectedPlanet.getDiameter()).isEqualTo(10465);
-        Assertions.assertThat(expectedPlanet.getClimate()).isEqualTo("arid");
-        Assertions.assertThat(expectedPlanet.getGravity()).isEqualTo("1 standard");
-        Assertions.assertThat(expectedPlanet.getTerrain()).isEqualTo("desert");
-        Assertions.assertThat(expectedPlanet.getSurfaceWater()).isEqualTo("1");
-        Assertions.assertThat(expectedPlanet.getPopulation()).isEqualTo("200000");
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(expectedPlanet.getName()).isEqualTo("Tatooine");
+        softly.assertThat(expectedPlanet.getRotationPeriod()).isEqualTo("23");
+        softly.assertThat(expectedPlanet.getOrbitalPeriod()).isEqualTo("304");
+        softly.assertThat(expectedPlanet.getDiameter()).isEqualTo(10465);
+        softly.assertThat(expectedPlanet.getClimate()).isEqualTo("arid");
+        softly.assertThat(expectedPlanet.getGravity()).isEqualTo("1 standard");
+        softly.assertThat(expectedPlanet.getTerrain()).isEqualTo("desert");
+        softly.assertThat(expectedPlanet.getSurfaceWater()).isEqualTo("1");
+        softly.assertThat(expectedPlanet.getPopulation()).isEqualTo("200000");
+        softly.assertAll();
     }
 }
